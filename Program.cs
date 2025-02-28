fd7e041efb8dc81b948ec4cb138222aa562fc158
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-28 14:38:37 -0500

    Starting on the project... a little-

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //What to do:
+            // - Get the Die Class Back
+            // - Make a multi-round loop that can end at any time
+            // - Make multiple if statements for the different bets
+            // - Make sure you can do invalid choices for the bet
+            // - Make a summary screen
+            // - Draw the dice.
         }
     }
 }
