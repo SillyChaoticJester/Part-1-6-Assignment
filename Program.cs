573bfad791513b38dd5df32f762b22ec66f9c980
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-28 14:11:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Part_1_6_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
