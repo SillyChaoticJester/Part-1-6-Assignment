b0e7f71659f08e7f118a992494bc28c9a29f4972
Miracle McMahon <339663114@GoTVDSB.ca>
2025-03-05 13:42:08 -0500

    Finished finally

--- Program.cs
+++ Program.cs
@@ -16,9 +16,9 @@
             Console.WriteLine("Welcome to the Dice Casino! Where you can bet all your money on dice.");
             Console.WriteLine("You'll be given five rounds, and you can quit after the end of each round if you wish.");
             
-            while (rounds != 6)
+            while (!done)
             {
-                while (!done)
+                while (rounds != 6)
                 {
                     Console.WriteLine();
                     Console.WriteLine("Here's all that you can bet on:");
@@ -49,7 +49,8 @@
                                 Console.WriteLine("Invalid Integer, please try again:");
                         }
                         Console.WriteLine();
-                        betTotal += userBet;
+                        die1.RollDie();
+                        die2.RollDie();
                         die1.DrawDie();
                         die2.DrawDie();
                         if (die1.Roll == die2.Roll)
@@ -57,6 +58,7 @@
                             Console.WriteLine();
                             Console.WriteLine("You got doubles! Congrats!");
                             userMoney += (userBet * 2);
+                            betTotal += (userBet * 2);
                         }
                         else
                         {
@@ -64,30 +66,9 @@
                             Console.WriteLine("Oops! You didn't get doubles!");
                             Console.WriteLine("You'll recieve half of your bet back.");
                             userMoney += (userBet / 2);
+                            betTotal += (userBet / 2);
                         }
                         rounds++;
-
-                        Console.WriteLine();
-                        Console.WriteLine("Would you like to continue?");
-                        choice = Console.ReadLine().ToLower().Trim();
-                        if (choice == "yes")
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine($"Ok! Here's how much money you have left: ${userMoney}");
-                            Console.WriteLine($"And here's how much you've won or lost: ${betTotal}");
-                        }
-                        else if (choice == "no")
-                        {
-                            done = true;
-                            Console.WriteLine();
-                            Console.WriteLine($"Ok! Here's how much money you have left: ${userMoney}");
-                            Console.WriteLine($"And here's how much you've won or lost: ${betTotal}");
-                        }
-                        else
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("That's not an option. Automatically continuing.");
-                        }
                     }
                     else if (betType == "even sum")
                     {
@@ -110,46 +91,28 @@
                         }
 
                         Console.WriteLine();
-                        betTotal += userBet;
+                        die1.RollDie();
+                        die2.RollDie();
                         die1.DrawDie();
                         die2.DrawDie();
                         Console.WriteLine();
                         Console.WriteLine("Your sum equals to " + (die1.Roll + die2.Roll));
-                        if (die1.Roll + die2.Roll % 2 == 0)
+                        if ((die1.Roll + die2.Roll) % 2 == 0)
                         {
                             Console.WriteLine();
                             Console.WriteLine("You got an Even Sum! Congrats! You won your bet!");
                             userMoney += userBet;
+                            betTotal += userBet;
                         }
                         else
                         {
                             Console.WriteLine();
                             Console.WriteLine("Oops! You didn't get an Even Sum! You lost your Bet!");
                             userMoney -= userBet;
+                            betTotal -= userBet;
                         }
                         rounds++;
 
-                        Console.WriteLine();
-                        Console.WriteLine("Would you like to continue?");
-                        choice = Console.ReadLine().ToLower().Trim();
-                        if (choice == "yes")
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine($"Ok! Here's how much money you have left: ${userMoney}");
-                            Console.WriteLine($"And here's how much you've won or lost: ${betTotal}");
-                        }
-                        else if (choice == "no")
-                        {
-                            done = true;
-                            Console.WriteLine();
-                            Console.WriteLine($"Ok! Here's how much money you have left: ${userMoney}");
-                            Console.WriteLine($"And here's how much you've won or lost: ${betTotal}");
-                        }
-                        else
-                        {
-                            Console.WriteLine();
-                            Console.WriteLine("That's not an option. Automatically continuing.");
-                        }
                     }
                     else if (betType == "odd sum")
                     {
@@ -174,25 +137,43 @@
                         }
 
                         Console.WriteLine();
-                        betTotal += userBet;
+                        die1.RollDie();
+                        die2.RollDie();
                         die1.DrawDie();
                         die2.DrawDie();
                         Console.WriteLine();
                         Console.WriteLine("Your sum equals to " + (die1.Roll + die2.Roll));
-                        if (die1.Roll + die2.Roll % 2 == 0)
+                        if ((die1.Roll + die2.Roll) % 2 == 0)
                         {
                             Console.WriteLine();
                             Console.WriteLine("Oops! You didn't get an Odd Sum! You lost your Bet!");
                             userMoney -= userBet;
+                            betTotal -= userBet;
                         }
                         else
                         {
                             Console.WriteLine();
                             Console.WriteLine("You got an Odd Sum! Congrats! You won your bet!");
                             userMoney += userBet;
+                            betTotal += userBet;
                         }
                         rounds++;
 
+                    }
+                    else
+                    {
+                        Console.WriteLine("That's not a choice");
+                    }
+
+                    if (rounds == 6)
+                    {
+                        done = true;
+                        Console.WriteLine("You've ran out of rounds");
+                        Console.WriteLine($"Here's how much money you have left: ${userMoney}");
+                        Console.WriteLine($"And here's how much you've won or lost: ${betTotal}");
+                    }
+                    else if (rounds != 6)
+                    {
                         Console.WriteLine();
                         Console.WriteLine("Would you like to continue?");
                         choice = Console.ReadLine().ToLower().Trim();
@@ -214,10 +195,6 @@
                             Console.WriteLine();
                             Console.WriteLine("That's not an option. Automatically continuing.");
                         }
-                    }
-                    else
-                    {
-                        Console.WriteLine("That's not a choice");
                     }
                 }
             }
